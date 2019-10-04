c82785cde035e7a5f6be17a39e0f7530ea32e40d
Mondher Khdhiri <mkhdhiri@hsph.harvard.edu>
2019-10-04 12:02:15 -0400

    anadama2

--- mkhdhiri/anadama2_teddy.py
+++ mkhdhiri/anadama2_teddy.py
@@ -38,21 +38,11 @@ workflow = Workflow(version="0.0.1", description="A workflow to run for the Tedd
 #in_files = workflow.get_input_files(extension=args.input_extension)
 #out_files = workflow.name_output_files(name=in_files, tag="metaphlan2_preprocessing")
 
-
 ### Section #4: Add tasks (Required)
 
-
-#workflow.add_task("./prep_species.py all_phlan.tsv Day")
-
-workflow.add_task("./prep_species.py [depends[0]] Day", depends=["all_phlan.tsv"])
-
-
-
 # 1- Keep major species 
-workflow.add_task("./prep_species.py all_phlan.tsv Day")
-# -----<> Outputs : 
-
-
+workflow.add_task("./prep_species.py [depends[0]] Day", depends=["all_phlan.tsv"])
+# -----<> Outputs : major_species.tsv | major_species_log10.tsv | species.tsv
 
 # 2- Calculating bray Curtis distance
 workflow.add_task ('./prep_bc_trends.py species.tsv init')
@@ -60,20 +50,73 @@ workflow.add_task ('./prep_bc_trends.py species.tsv init')
 # ----<> Outputs : bc_dists_init.details.tsv | bc_dists_init.tsv
 
 
-
 # 3- Compute Species acquisition dates
 workflow.add_task ('./prep_acquisitions.py species.ts')
 # ----<> Outputs : acquisitions.tsv
 
+# 4- Compute the diversity indices
+workflow.add_task ('./drop_rows.py -i species.tsv -rm Subject Day -o species_no_meta.tsv')
+workflow.add_task ('./diversity.R -i species_no_meta.tsv -o Diversity_indices.tsv')
+# ----<> Outputs : Diversity_indices.tsv (species_no_meta.tsv will be removed)
+
+
+#___________________________________________________
+### Step 2 : preprocessing of the genetics data    .
+#---------------------------------------------------
+# Cleaning
+
+workflow.add_task ('./prep_genetics.py snps.ped snps.map species.tsv')
+# ----<> Outputs : genetics.tsv
+####!!!!!!! only 877 subject are present in the genetics.tsv file while the species.tsv contains a list of 12152 subject   ===> Not all; the subjects have genetics data 
+#### !!!!! Why the use of the species at this step ! ! 
+
+### The PC was generated using the Plink 
+
+workflow.add_task ('plink --file snps --pca header tabs var-wts') 
+
+
+
+#___________________________________________________
+### Step 3 : Modeling                              .
+#---------------------------------------------------
+#
+
+# ! The modeling is based on the bray curtis distance rather than the genetics file per se : where is the script to generate the PC from the genetics data or should I generate my own    === > Bc_trends script or bug script (the model could function witn either 
+
+)
+# ! The model is in the format of model = lmer.lmer( "Bug ~ Day + SNP + Day * SNP + (1|Subject)" )  do not understand the argument (1|Subject)
+# !  
+#!!! Why the model was run in chunks ? the use of results.py script what it will add , to what purpose it was used ???
+
+
+
+workflo.add_task ('./model_rpy2.py species.tsv genetics_pca.tsv results')
 
 
 
 
 
+#!!! The results  little expl 
 
-#workflow.add_task(
- #   "metaphlan2.py [depends[0]] --input_type fastq --no_map > [targets[0]]",
-  #  depends=["sample1.fastq"], targets=["sample1_profile.txt"])
+
+
+#___________________________________________________
+### Step 4 : Result representation                 .
+#---------------------------------------------------
+#
+
+# ! How the results were parsed -- Specific parsing script that has already been written -- Or I will write my own ?
+# ! what are the scripts used for the results??
+
+
+
+
+#### Other questions : Enrich PCA : script enrich.py : for what purpose was it used 
+
+
+
+
+
 
 
 ### Section #5: Run tasks (Required)
