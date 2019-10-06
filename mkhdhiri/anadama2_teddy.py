0886ecefedabb4598788fd4cee12024e62241407
Mondher Khdhiri <mkhdhiri@hsph.harvard.edu>
2019-10-06 00:38:04 -0400

    anadama2

--- mkhdhiri/anadama2_teddy.py
+++ mkhdhiri/anadama2_teddy.py
@@ -1,18 +1,15 @@
 ### To import Anadama2 (for python2)
 import sys
 sys.path.insert (1, '/n/huttenhower_lab/data/teddy_genetics/mondher_analysis/py3venev/lib_py2')
-
+# pip install --target= /n/huttenhower_lab/data/teddy_genetics/mondher_analysis/py3venev/lib_py2 rpy2
 """
 This script is written to run the Teddy2 analysis as an Anadama2 workflow
 """
-
 import anadama2
 from anadama2 import Workflow
 
 ### Initializing a workflow instance with Anadama2
-#workflow = Workflow(version="0.0.1", description="A workflow to run for the Teddy2 project", remove_options=["input","output"])
-
-workflow = Workflow(version="0.0.1", description="A workflow to run for the Teddy2 project")
+workflow = Workflow(version="0.0.1", description="A workflow to run for the Teddy2 project", remove_options=["input","output"])
 
 #### Steps of the workflow to be run using Anadama2
 #___________________________________________________
@@ -24,60 +21,37 @@ workflow = Workflow(version="0.0.1", description="A workflow to run for the Tedd
 ## Command to run : ./prep_species.py all_phlan.tsv Day
 ## 
 ## The output is : major_species.tsv, major_species_log10.tsv, mp201_sample_mapping.tsv
-
-
 # 1- Keep major species 
-workflow.add_task("./prep_species.py [depends[0]] Day", depends=["all_phlan.tsv"])
+workflow.do("./prep_species.py all_phlan.tsv Day")
 # -----<> Outputs : major_species.tsv | major_species_log10.tsv | species.tsv
-
 # 2- Calculating bray Curtis distance
-#workflow.add_task ('./prep_bc_trends.py species.tsv init')
-## ???? What is the difference between the init and the prev ??
+workflow.do ('./prep_bc_trends.py species.tsv init')
 # ----<> Outputs : bc_dists_init.details.tsv | bc_dists_init.tsv
 
-
 # 3- Compute Species acquisition dates
-#workflow.add_task ('./prep_acquisitions.py species.ts')
+workflow.do ('./prep_acquisitions.py species.tsv')
 # ----<> Outputs : acquisitions.tsv
 
 # 4- Compute the diversity indices
-#workflow.add_task ('./drop_rows.py -i species.tsv -rm Subject Day -o species_no_meta.tsv')
-#workflow.add_task ('./diversity.R -i species_no_meta.tsv -o Diversity_indices.tsv')
+workflow.do ('./drop_rows.py -i species.tsv -rm Subject Day -o species_no_meta.tsv')
+workflow.do ('./diversity.R -i species_no_meta.tsv -o Diversity_indices.tsv')
 # ----<> Outputs : Diversity_indices.tsv (species_no_meta.tsv will be removed)
-
-
 #___________________________________________________
 ### Step 2 : preprocessing of the genetics data    .
 #---------------------------------------------------
 # Cleaning
-
-#workflow.add_task ('./prep_genetics.py snps.ped snps.map species.tsv')
+workflow.do ('./prep_genetics.py snps.ped snps.map species.tsv')
 # ----<> Outputs : genetics.tsv
-####!!!!!!! only 877 subject are present in the genetics.tsv file while the species.tsv contains a list of 12152 subject   ===> Not all; the subjects have genetics data 
-#### !!!!! Why the use of the species at this step ! ! 
-
 ### The PC was generated using the Plink 
-
-#workflow.add_task ('plink --file snps --pca header tabs var-wts') 
-
-
+workflow.do ('plink --file snps --pca header tabs var-wts') 
+### Clean The raw plink output
+workflow.do ('./clean_plink.py -i plink.eigenvec -o genetics_pca.tsv')
 
 #___________________________________________________
 ### Step 3 : Modeling                              .
 #---------------------------------------------------
 #
-
-# ! The modeling is based on the bray curtis distance rather than the genetics file per se : where is the script to generate the PC from the genetics data or should I generate my own    === > Bc_trends script or bug script (the model could function witn either 
-
-# ! The model is in the format of model = lmer.lmer( "Bug ~ Day + SNP + Day * SNP + (1|Subject)" )  do not understand the argument (1|Subject)
-# !  
-#!!! Why the model was run in chunks ? the use of results.py script what it will add , to what purpose it was used ???
-
-# workflow.add_task ('./model_rpy2.py species.tsv genetics_pca.tsv results')
-
-#!!! The results  little expl 
-
-
+workflow.do ('./model_rpy2.py species.tsv genetics_pca.tsv results')
 #___________________________________________________
 ### Step 4 : Result representation                 .
 #---------------------------------------------------
@@ -85,10 +59,6 @@ workflow.add_task("./prep_species.py [depends[0]] Day", depends=["all_phlan.tsv"
 
 
 #||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-
-
-
-### Work on generic Anadama2 script during the lab meet 
 
 # 1  Focus on the parsing of the data generated through the model in step 3 in order to generate the needed results.
 
@@ -108,8 +78,6 @@ workflow.add_task("./prep_species.py [depends[0]] Day", depends=["all_phlan.tsv"
 
 
 #### Other questions : Enrich PCA : script enrich.py : for what purpose was it used 
-
-
 
 ### Section #5: Run tasks (Required)
 workflow.go()
