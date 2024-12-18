7f858e9bde299b9aadc6b9e1e8931962feb7bc7b
DanyueDong62 <nhddg@channing.harvard.edu>
2024-12-18 16:13:06 -0500

    Archived File

--- src_franzosa/plots/heatmap_bug_pc/run.sh
+++
@@ -1,4 +0,0 @@
-python bug_pc_heatmap.py ../model_results/major_species.genetics_pca.results.txt add
-python bug_pc_heatmap.py ../model_results/major_species.genetics_pca.results.txt int
-python bug_pc_heatmap.py ../model_results/major_species.genetics_pca.results.txt add --simplify
-python bug_pc_heatmap.py ../model_results/major_species.genetics_pca.results.txt int --simplify
